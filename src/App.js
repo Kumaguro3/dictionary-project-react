356b3c7e48cb94eea3258e9d89b8849eb6d37513
Kumaguro3 <lily.sergent@gmail.com>
2022-09-26 16:43:05 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Hello ! </h1>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
