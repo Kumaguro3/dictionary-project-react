09d409833096eb0b6901657bef2d0952961a4245
Kumaguro3 <lily.sergent@gmail.com>
2022-09-27 21:29:45 +0900

    Arranged structure between words

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Synonyms from "./Synonyms.js";
+import Synonyms from "./Synonyms";
 import "./Dictionary.css";
 
 export default function Meaning(props) {
@@ -15,9 +15,7 @@ export default function Meaning(props) {
           <div key={index}>
             <div className="search-word-definition">
               <strong className="definition-text">Definition:</strong>{" "}
-              {definition.definition}
-              <br />
-              <strong className="examples-text">Examples:</strong>
+              <p> {definition.definition} </p>
               <em>{definition.example}</em>
               <div className="synonyms-text">
                 {" "}
