a6b06de7945918783e533e8e1c988a0bd6ac981d
Kumaguro3 <lily.sergent@gmail.com>
2022-09-28 21:12:36 +0900

    Changed text-styling and color

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,23 +1,24 @@
 import React from "react";
 import Synonyms from "./Synonyms";
 import "./Dictionary.css";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="meaning">
-      <h3 className="verb-adj-noun">
+      <h2 className="verb-adj-noun">
         {" "}
         <strong>{props.meaning.partOfSpeech}</strong>
-      </h3>
+      </h2>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <div className="search-word-definition">
-              <div className="definition-text">Definition</div>{" "}
-              <p> {definition.definition} </p>
-              <em>{definition.example}</em>
-              <div className="synonyms-text">
+              <div className="definition">Definition</div>{" "}
+              <p className="definition-text"> {definition.definition} </p>
+              <p className="example-text">{definition.example}</p>
+              <div className="synonym-text">
                 {" "}
                 <Synonyms synonyms={definition.synonyms} />
               </div>
